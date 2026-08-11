ac38829ae53f57cfcd28c443bb3c19558d21d02c
rodrigocorrea10 <rodrigocorea1503@gmail.com>
2026-08-11 19:58:47 -0300

    data e mes pt-br

--- views/dashboard.py
+++ views/dashboard.py
@@ -1,3 +1,9 @@
+import locale
+
+try:
+    locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
+except locale.Error:
+    locale.setlocale(locale.LC_TIME, "Portuguese_Brazil.1252")
 import customtkinter as ctk
 from PIL import Image
 import os
